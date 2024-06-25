374e4acc37f20c7f10283f8756e6986461426b20
shagun <shagun@gmail.com>
2024-06-25 14:03:57 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SHAGUN</div>
+          <div className="right">
+            <p>Sikar,Rajasthan</p>
+            <p>Open: 10:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By SHAGUNSHARMA</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By shagun.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
