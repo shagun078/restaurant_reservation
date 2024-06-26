0e64c53ed67d61896489c8b663cc8dccb1747021
shagun <shagun@gmail.com>
2024-06-26 16:15:55 +0530

    route changes

--- backend/app.js
+++ backend/app.js
@@ -8,24 +8,31 @@ import reservationRouter from "./routes/reservationRoute.js";
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+console.log("Allowed Frontend URL:", process.env.FRONTEND_URL);
 
 // CORS configuration
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL], 
+    origin: process.env.FRONTEND_URL, 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
     preflightContinue: false,
-    optionsSuccessStatus: 204
+    optionsSuccessStatus: 204,
   })
 );
 
-// Handle preflight requests
-app.options("*", cors());
+// Middleware to log requests and headers for debugging
+app.use((req, res, next) => {
+  console.log("Request Headers:", req.headers);
+  next();
+});
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use("/api/v1/reservation", reservationRouter);
 
 dbConnection();
